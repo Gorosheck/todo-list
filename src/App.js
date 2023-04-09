7f701e4c83f71ee9afe21b98940a19b6934065fb
Kirill Goroshko <gorosheck@gmail.com>
2023-04-09 11:37:29 +0300

    add html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='header-title'><h1>ToDo List</h1></div>
       </header>
+      <hr />
+      <body>
+        <div className='todo-body-wrapper'>
+          <div>
+            <textarea placeholder='add your new todo item' rows='3' cols='100' />
+          </div>
+          <div>
+            <button className='button-add-item'>ADD item</button>
+          </div>
+        </div>
+        <hr />
+        <div className='todo-items-wrapper'>
+          <div className='todo-item'>
+
+          </div>
+        </div>
+        <hr />
+      </body>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let state = {
+  itemsActive: [
+    { id: 1, message: 'first to-do' },
+    { id: 1, message: 'second to-do' },
+    { id: 1, message: 'first to-do' },
+    { id: 1, message: 'first to-do' }
+  ],
+  itemsDone: [
+    { id: 1, message: 'first to-do' }
+  ],
+  itemsDelete: [
+    { id: 1, message: 'first to-do' }
+  ],
+  newItem: ""
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +28,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
