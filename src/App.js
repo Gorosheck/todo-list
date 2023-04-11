504c129e8f0f726338d95a8fb74838fdac309869
Kirill Goroshko <gorosheck@gmail.com>
2023-04-11 22:18:42 +0300

    add onclick and ref

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
 import './App.css';
 import Item from './Item';
 
 const App = (props) => {
 
   let todoItems = props.state.map(element => <Item todoItem={element.message} />);
+
+  let newItemElement = React.createRef();
+
+  const addItem = (props) => {
+    let text = newItemElement.current.value;
+
+  }
 
   return (
     <div className="app">
@@ -14,10 +22,10 @@ const App = (props) => {
       <body>
         <div className='todo-body-wrapper'>
           <div>
-            <textarea placeholder='add your new todo item' rows='3' cols='100' />
+            <textarea ref={newItemElement} placeholder='add your new todo item' rows='3' cols='100' />
           </div>
           <div>
-            <button className='button-add-item'>ADD item</button>
+            <button className='button-add-item' onClick={addItem}>ADD item</button>
           </div>
         </div>
         <hr />
